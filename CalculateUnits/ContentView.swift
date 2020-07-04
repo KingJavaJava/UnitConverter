dbce3fa24dcd471c7aa0b28b18630960db47db21
hareiwaja <hareiwaja@yahoo.com>
2020-07-03 22:54:23 -0500

    Initial Commit

---
+++ CalculateUnits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculateUnits
+//
+//  Created by Harei Waja on 7/3/20.
+//  Copyright © 2020 Rex Orioko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
