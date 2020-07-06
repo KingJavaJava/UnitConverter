b1c6cd20b24ba0ef641c85d09bfc566fbff989fd
hareiwaja <hareiwaja@yahoo.com>
2020-07-06 01:44:11 -0500

    Succesfully wrote logic for computed property to grab string from array

--- CalculateUnits/ContentView.swift
+++ CalculateUnits/ContentView.swift
@@ -10,26 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var InputUnit = ""
-    @State private var OutputUnit = ""
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
     @State private var inputNumber = ""
     
     let possibleUnits = ["sec","min","hrs"]
     
+    //Grab state value index number of array and return string in array
+    var viewInputUnit:String  {
+        let enteredUnit = inputUnit
+        let text = possibleUnits[enteredUnit]
+        return text
+    }
+    var viewOutputUnit:String  {
+        let enteredUnit = outputUnit
+        let text = possibleUnits[enteredUnit]
+        return text
+    }
 
+    
+    
     
     
     var body: some View {
         NavigationView {
             Form {
-                Picker("Select Unit", selection: $InputUnit) {
-                    ForEach(0 ..< possibleUnits.count){number in
-                        Text("\(self.possibleUnits[number])")
+                Section {
+                    Picker("Select Unit", selection: $inputUnit) {
+                        ForEach(0 ..< possibleUnits.count){number in
+                            Text("\(self.possibleUnits[number])")
+                        }
                     }
-                }
-                Picker("Convert To", selection: $OutputUnit) {
-                    ForEach(0 ..< possibleUnits.count){number in
-                        Text("\(self.possibleUnits[number])")
+                    Picker("Convert To", selection: $outputUnit) {
+                        ForEach(0 ..< possibleUnits.count){number in
+                            Text("\(self.possibleUnits[number])")
+                        }
                     }
                 }
                 Section(header: Text("Enter The Number")) {
@@ -37,7 +52,7 @@ struct ContentView: View {
                         .keyboardType(.decimalPad)
                 }
                 Section(header: Text("Result")) {
-                    Text("")
+                    Text("$\(viewInputUnit) : \(viewOutputUnit)")
                 }
             }
         .navigationBarTitle("Unit Converter")
